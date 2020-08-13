33ead21e1b59ee6aa25eb281d5de94eb802eff60
Shubham Apoorva Chandra <apoorvshubham@gmail.com>
2020-08-14 03:31:32 +0530

    cookie

--- app.js
+++ app.js
@@ -31,13 +31,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req,res,next){
-	console.log(req.headers);
-
-	var authHeader = req.headers.authorization;
-
+	console.log(req.signedCookies);
+	if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
 	if(!authHeader){
 		varerr =new Error('you are not authorised');
 	 res.setHeader('WWW-Authenticate', 'Basic');
@@ -49,6 +48,7 @@ function auth(req,res,next){
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
+  	res.cookie('user','admin',{signed:true})
       next(); // authorized
   } else {
       var err = new Error('You are not authenticated!');
@@ -57,6 +57,17 @@ function auth(req,res,next){
       next(err);
   }
 }
+else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+  }
 
 app.use(auth);
 
