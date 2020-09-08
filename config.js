c5f14d33660abba861e3ef3e0d44fcfa82593a17
apoorv-bit <33101720+apoorv-bit@users.noreply.github.com>
2020-09-09 02:32:06 +0530

    Update config.js
    removed authentication details

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '589539578389531',
-        clientSecret: '2ec53dd0b14e8d96630e5be05bf11ae9'
+        clientId: '---------------',
+        clientSecret: '---------------------------'
 }}
